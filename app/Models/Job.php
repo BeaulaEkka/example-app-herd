e1428c2ef19ac9cc9e70573e6daff0b9238507c3
Your Name <ekka.beaula@gmail.com>
2024-09-07 23:15:30 +0200

    tags pivot table

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -7,8 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Job extends Model
 {
-
-    protected $table = 'job_listings';
-    protected $fillable = ['title', 'salary'];
     use HasFactory;
+    protected $table = 'job_listings';
+    protected $fillable = ['title', 'salary',];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class,foreignPivotKey:'job_listing_id');
+    }
 }
