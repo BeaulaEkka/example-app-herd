8ab94f791c102087c9e2a0c168806548d8fd7e91
Your Name <ekka.beaula@gmail.com>
2024-09-14 13:43:03 +0200

    installed laravel debug manager

--- resources/views/job.blade.php
+++ resources/views/job.blade.php
@@ -3,6 +3,7 @@
         Job Details
     </x-slot:heading>
     <h2 class="font-semibold text-blue-600 text-xl mb-4">{{ $job['title'] }}</h2>
+    <h3><strong>Company: </strong>{{ $job->employer->name }}</h3>
     <p>This job pays {{ $job['salary'] }} per year.</p>
     <h3 class="font-bold text-md py-2">Job Description:</h3>
     <p>{{ $job['description'] }} </p>
@@ -12,7 +13,7 @@
         @if ($job->tags)
 
             @foreach ($job->tags as $tag)
-                <p class="px-4 py-2 bg-blue-100  inline-flex rounded-md   shadow-sm capitalize">
+                <p class="px-4 py-2 border border-gray-300  inline-flex rounded-md   shadow-sm capitalize">
                     {{ $tag->name }}</p>
             @endforeach
 
--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -4,11 +4,13 @@
     </x-slot:heading>
     <div>
         @foreach ($jobs as $job)
-            <li>
-                <a href="/jobs/{{ $job['id'] }}">
+            <div class="my-5 hover:bg-gray-200 rounded-md cursor-pointer">
+                <a href="/jobs/{{ $job['id'] }}" class=" block px-4 py-6 border border-gray-200 rounded-md">
+                    <div class=" text-blue-600 font-bold">{{ $job->employer->name }}</div>
                     <strong>{{ $job['title'] }}:</strong> Pays €{{ $job['salary'] }} per year.
-            </li>
-            </a>
+
+                </a>
+            </div>
         @endforeach
         </ul>
 </x-layout>
